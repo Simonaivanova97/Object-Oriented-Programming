e4c6be9532669dd5901c00a4a9279d471ca1221e
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-25 12:52:27 +0200

    Add Book.hpp

---
+++ MultipleInheritance/PrintEditions/Book.hpp
@@ -0,0 +1,21 @@
+#ifndef Book_hpp
+#define Book_hpp
+
+#include <stdio.h>
+#include "PrintEdition.hpp"
+
+class Book : virtual public PrintEdition{
+public:
+    Book();
+    Book(const char*, int, double, const char*);
+    Book(const Book&);
+    Book& operator=(const Book&);
+    virtual ~Book();
+    
+    virtual void print() const override;
+
+private:
+    char* author;
+    
+    void copyBook(const Book&);
+};
