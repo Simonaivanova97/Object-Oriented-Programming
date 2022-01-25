dab8c2fdaa6783d1198b0034eb06a627bfe03330
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-25 12:44:35 +0200

    Add Expert.hpp

---
+++ ExamLevel5/Expert.hpp
@@ -0,0 +1,20 @@
+#ifndef Expert_hpp
+#define Expert_hpp
+
+#include <stdio.h>
+#include "Employee.hpp"
+
+class Expert : public Employee{
+public:
+    Expert();
+    Expert(const char*, int, int);
+    
+    virtual void print() const override;
+    virtual bool execute(const char*) override;
+    virtual double successRate() const override;
+    virtual Employee* clone() override;
+
+    friend ostream& operator<<(ostream&, const Expert&);
+private:
+};
+#endif /* Expert_hpp */
