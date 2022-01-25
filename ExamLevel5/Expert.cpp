6081ab882658ab2e6ede312bf6b97fe12062dfc7
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-25 12:44:44 +0200

    Add Expert.cpp

---
+++ ExamLevel5/Expert.cpp
@@ -0,0 +1,24 @@
+#include "Expert.hpp"
+Expert::Expert() : Employee() {}
+Expert::Expert(const char* n, int nT, int sT) : Employee(n, nT, sT) {}
+bool Expert::execute(const char* task){
+    numberOfExecuteTasks++;
+    successfullyCompletedTasks++;
+    return true;
+}
+double Expert::successRate() const{
+    if(numberOfExecuteTasks==0){
+        return 0;
+    }
+    return (double)successfullyCompletedTasks/numberOfExecuteTasks;
+}
+Employee* Expert::clone(){
+    return new Expert(*this);
+}
+void Expert::print() const{
+    cout<<*this<<endl;
+}
+ostream& operator<<(ostream& out, const Expert& e){
+    out<<"expert: "<<e.getName();
+    return out;
+}
