443b2d245c20835d737e13455c5418bc61b6612a
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-25 12:44:06 +0200

    Add Worker.hpp

---
+++ ExamLevel5/Worker.hpp
@@ -0,0 +1,22 @@
+#ifndef Worker_hpp
+#define Worker_hpp
+
+#include <stdio.h>
+#include "Employee.hpp"
+
+class Worker : public Employee{
+public:
+    Worker();
+    Worker(const char*, int, int, int);
+    
+    virtual bool execute(const char*) override;
+    virtual double successRate() const override;
+    virtual Employee* clone() override;
+    
+    virtual void print() const override;
+    friend ostream& operator<<(ostream&, const Worker&);
+private:
+    int k;
+    int currentExecuteTasks;
+    
+};
