ffede9080c5a406c701917fcbb346c861702e906
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-25 12:53:41 +0200

    Add Manga.cpp

---
+++ MultipleInheritance/PrintEditions/Manga.cpp
@@ -0,0 +1,12 @@
+#include "Manga.hpp"
+Manga::Manga() : PrintEdition(), Book(), Magazine(){}
+Manga::Manga(const char* t, int y, double p, const char* a, Periodicity per) : PrintEdition(t, y, p), Book(t, y, p, a), Magazine(t, y, p, per){}
+void Manga::print() const{
+    //PrintEdition::print();
+    Book::print();
+    //Magazine::print();
+    Magazine::printOwlyPeriodicity();
+}
+bool Manga::weeklyManga() const{
+    return getPeriod()==Periodicity::Weekly;
+}
