b16fd3afb85c1ffccdf2d22b827bb5f4023e85bb
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-26 15:50:34 +0200

    Delete unnecessary, after changing task condition

--- MultipleInheritance/PrintEditions/Magazine.hpp
+++ MultipleInheritance/PrintEditions/Magazine.hpp
@@ -12,16 +12,14 @@ class Magazine : virtual public PrintEdition{
 public:
     Magazine();
     Magazine(const char*, int, double, Periodicity);
-    virtual ~Magazine();
     
-    virtual void print() const override;
-    virtual PrintEdition* clone() override;
+    void print() const;
     
 private:
     Periodicity period;
     const char* periodicities[3]={"Weekly", "Monthly", "Yearly"};
+    
 protected:
     void printOwlyPeriodicity() const;
-    const Periodicity getPeriod() const;
 };
 #endif /* Magazine_hpp */
