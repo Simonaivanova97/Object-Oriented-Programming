eb2502abaf11acdfb1270ee0282ddb0bdb707922
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-28 12:40:17 +0200

    Add Manga.hpp

---
+++ VirtualFunctions.Polymorphism.AbstractClasses/PrintEditions/Manga.hpp
@@ -0,0 +1,16 @@
+#ifndef Manga_hpp
+#define Manga_hpp
+
+#include <stdio.h>
+#include "Book.hpp"
+#include "Magazine.hpp"
+
+class Manga : public Book, public Magazine{
+public:
+    Manga();
+    Manga(const char*, int, double, const char*, Periodicity);
+    
+    virtual void print() const override;
+
+};
+#endif /* Manga_hpp */
