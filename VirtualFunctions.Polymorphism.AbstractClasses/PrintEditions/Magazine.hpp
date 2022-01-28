21bb3d2510030c247ecef117bd3dcfe035371803
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-28 12:37:53 +0200

    Add Magazine.hpp

---
+++ VirtualFunctions.Polymorphism.AbstractClasses/PrintEditions/Magazine.hpp
@@ -0,0 +1,25 @@
+#ifndef Magazine_hpp
+#define Magazine_hpp
+
+#include <stdio.h>
+#include "PrintEdition.hpp"
+
+enum class Periodicity{
+    Weekly, Monthly, Yearly
+};
+
+class Magazine : virtual public PrintEdition{
+public:
+    Magazine();
+    Magazine(const char*, int, double, Periodicity);
+    
+    virtual void print() const override;
+    
+private:
+    Periodicity period;
+    const char* periodicities[3]={"Weekly", "Monthly", "Yearly"};
+    
+protected:
+    void printOwlyPeriodicity() const;
+};
+#endif /* Magazine_hpp */
