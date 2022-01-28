a138370218d2ec98dcd29c42b49422dd551ec0c7
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-28 16:54:14 +0200

    Add Magazine.cpp

---
+++ CollectionsOfObjectsInPolymorphicHierarchy/PrintEditions/Magazine.cpp
@@ -0,0 +1,21 @@
+#include "Magazine.hpp"
+Magazine::Magazine() : PrintEdition(){
+    period=Periodicity::Weekly;
+}
+Magazine::Magazine(const char* t, int y, double p, Periodicity per) : PrintEdition(t, y, p){
+    period=per;
+}
+Magazine::~Magazine(){}
+void Magazine::print() const{
+    PrintEdition::print();
+    cout<<"The magazine has periodicity: "<<periodicities[static_cast<int>(period)]<<endl;
+}
+void Magazine::printOwlyPeriodicity() const{
+    cout<<"The magazine has periodicity: "<<periodicities[static_cast<int>(period)]<<endl;
+}
+const Periodicity Magazine::getPeriod() const{
+    return period;
+}
+PrintEdition* Magazine::clone(){
+    return new Magazine(*this);
+}
