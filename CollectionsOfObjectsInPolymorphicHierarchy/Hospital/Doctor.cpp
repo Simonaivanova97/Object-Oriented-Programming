003c9e3590cf1a2790d80b02857b77beaf854a6e
Simona Ivanova <simona02ivanova@gmail.com>
2022-01-28 21:31:40 +0200

    Add Hospital.cpp

---
+++ CollectionsOfObjectsInPolymorphicHierarchy/Hospital/Doctor.cpp
@@ -0,0 +1,40 @@
+#include "Doctor.hpp"
+void Doctor::copyDoctor(const Doctor& other){
+    name=new char[strlen(other.name)+1];
+    assert(name!=NULL);
+    strcpy(name, other.name);
+    experience=other.experience;
+}
+Doctor::Doctor(){
+    name=new char[strlen("Unknown")+1];
+    assert(name!=NULL);
+    strcpy(name, "Unknown");
+    experience=0;
+}
+Doctor::Doctor(const char* n, int e){
+    name=new char[strlen(n)+1];
+    assert(name!=NULL);
+    strcpy(name, n);
+    experience=e;
+}
+Doctor::Doctor(const Doctor& other){
+    copyDoctor(other);
+}
+Doctor& Doctor::operator=(const Doctor& other){
+    if(this!=&other){
+        delete []name;
+        copyDoctor(other);
+    }
+    return *this;
+}
+Doctor::~Doctor(){
+    delete []name;
+}
+int Doctor::getSalary() const{
+    return experience*2+500;
+}
+void Doctor::print() const{
+    cout<<"Name: "<<name<<endl;
+    cout<<"Experience: "<<experience<<endl;
+}
+
